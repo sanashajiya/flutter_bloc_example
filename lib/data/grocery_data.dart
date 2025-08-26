259816efb9793ca03d54088590d6af30b264b44d
sanashajiya <ssanashajiya@gmail.com>
2025-08-26 18:59:22 +0530

    initial setup for flutter bloc project

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,44 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Bananas',
+      'description': 'A bunch of ripe bananas',
+      'price': 0.99,
+      'imageUrl':
+          'https://m.media-amazon.com/images/I/51ebZJ+DR4L._UF1000,1000_QL80_.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Apples',
+      'description': 'A bag of fresh apples',
+      'price': 1.49,
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCQVgc8Z4MuUUMZRdNf0qGvDgWu016u02ARw&s',
+    },
+    {
+      'id': 3,
+      'name': 'Milk',
+      'description': 'A gallon of whole milk',
+      'price': 2.99,
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpZ2fMBpaHCCAxNoyrfUuK5y-bsjfqGO7v8A&s',
+    },
+    {
+      'id': 4,
+      'name': 'Bread',
+      'description': 'A loaf of whole wheat bread',
+      'price': 1.99,
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkOAIl9N99juE0LtiIPDWenkHcPr714Uh5sg&s',
+    },
+    {
+      'id': 5,
+      'name': 'Eggs',
+      'description': 'A dozen fresh eggs',
+      'price': 1.99,
+      'imageUrl':
+          'https://media.post.rvohealth.io/wp-content/uploads/2020/09/health-benefits-of-eggs-732x549-thumbnail-732x549.jpg',
+    },
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_example/features/home/ui/home.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bloc',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
